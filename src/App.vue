549ceaa39455c2c1dbb0be2f337734160d84f139
Cameron <github@strixdesigns.com>
2020-05-23 23:55:39 -0700

    add styling

--- src/App.vue
+++ src/App.vue
@@ -24,15 +24,21 @@
     }
 </script>
 
-<style>
+<style lang="scss">
 
     /*@import "./assets/sass/styles.scss";*/
     @import url('https://fonts.googleapis.com/css?family=Nunito&display=swap');
-
+    $primary: hsl(261, 62%, 29%);
+    @import "~bulma";
     * {
         font-family: 'Nunito', sans-serif;
+
     }
     hamburger{
         margin-bottom: 3.25rem;
+    }
+    a.dropdown-item.is-active{
+        background-color: $primary;
+        color: white;
     }
 </style>
